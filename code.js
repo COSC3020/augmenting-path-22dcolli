fa284faa558a82289cbf88c85c0bd1bd8e0c80b5
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-12-05 02:06:02 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,50 @@
 function augmentingPath(graph, start, end) {
-    return [];
+
+    let vistited = [];
+    let path = [];
+
+    //happen to start at the end node
+    if(start === end){
+        return [start];
+    }
+
+    for(let node in graph){
+        vistited[node] = false;
+    }
+
+    function backtrack(curNode){
+
+        path.push(curNode);
+        vistited[curNode] = true;
+
+        if(curNode == end){
+            return true;
+        }
+
+        for(let neighbor in graph[curNode]){
+
+            //neighbor not visited
+            if(!vistited[neighbor]){
+
+                //recurse on neighbor
+                if(backtrack(neighbor)){
+                    //path found
+                    return true;
+                }
+
+            }
+        }
+
+        path.pop();
+        visited[curNode] = false;
+        return false;
+    }
+
+    if (backtrack(start)) {
+        //if theres a path it returns
+        return path; 
+    } else {
+        //no path comes back empty
+        return [];
+    }
 }
